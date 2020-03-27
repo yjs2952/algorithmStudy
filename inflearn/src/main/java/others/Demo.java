bb59fb9053c795d7b38076e3968e985ebf5a1fbf
yjs <yjs2952@naver.com>
2020-03-27 11:17:55 +0900

    알고리즘 최대공약수, 최소공배수

--- inflearn/src/main/java/others/Demo.java
+++ inflearn/src/main/java/others/Demo.java
@@ -1,4 +1,4 @@
-package problem_11_to_20;
+package others;
 
 import java.util.Arrays;
 
---
+++ inflearn/src/main/java/others/GcdLcm.java
@@ -0,0 +1,31 @@
+package others;
+
+import java.util.Arrays;
+
+public class GcdLcm {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(3, 12)));
+        System.out.println(Arrays.toString(solution(2, 5)));
+    }
+
+    public static int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        answer[0] = gcd(n, m);
+        answer[1] = lcm(n, m);
+        return answer;
+    }
+
+    public static int gcd(int a, int b){
+        while (b > 0)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp%b;
+        }
+        return a;
+    }
+
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
