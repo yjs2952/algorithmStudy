af4467739ce5e33c9fb5cf42d503cfe248bb5d1c
yjs2952 <yjs2952@naver.com>
2020-12-23 00:25:27 +0900

    구현 문제 2 풀이 추가

--- inflearn/src/main/java/this_is_coding_test/ch12/Q2.java
+++ inflearn/src/main/java/this_is_coding_test/ch12/Q2.java
@@ -9,27 +9,48 @@ public class Q2 {
 
         System.out.println(solution(input));
         System.out.println(solution(input1));
+        System.out.println(solution2(input));
+        System.out.println(solution2(input1));
     }
 
     private static String solution(String input) {
         String[] split = input.split("");
         Arrays.sort(split);
         input = String.join("", split);
-
         StringBuilder sb = new StringBuilder(input);
 
-        int length = input.length();
+        int length = sb.length();
         int sum = 0;
-
         for (int i = 0; i < length; i++) {
-            char letter = input.charAt(i);
-            if (letter >= '0' && letter <= '9') {
+            char letter = sb.charAt(i);
+            if (Character.isDigit(letter)) {
                 sum += Character.digit(letter, 10);
-                sb.deleteCharAt(i);
+                sb.setCharAt(i, ' ');
+            } else {
+                break;
             }
         }
 
+
+        if (sum > 0) sb.append(sum);
+
+        return sb.toString().trim();
+    }
+
+    private static String solution2(String input) {
+        char[] chars = input.toCharArray();
+        Arrays.sort(chars);
+
+        int sum = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isDigit(chars[i])) {
+                sum += Character.digit(chars[i], 10);
+                chars[i] = ' ';
+            }
+        }
+
+        StringBuilder sb = new StringBuilder(String.valueOf(chars));
         sb.append(sum);
-        return sb.toString();
+        return sb.toString().trim();
     }
 }
