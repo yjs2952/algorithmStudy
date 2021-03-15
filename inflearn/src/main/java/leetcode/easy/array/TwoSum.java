e84e2b1bdfbd474c34dcc8e17467d7bc7e6c1285
yjs2952 <yjs2952@naver.com>
2021-03-15 22:52:09 +0900

    leetcode twosum

---
+++ inflearn/src/main/java/leetcode/easy/array/TwoSum.java
@@ -0,0 +1,30 @@
+package leetcode.easy.array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums1 = {2, 7, 11, 15};
+        int[] nums2 = {3, 2, 4};
+        int[] nums3 = {3, 3};
+
+        System.out.println(Arrays.toString(twoSum(nums1, 9)));
+        System.out.println(Arrays.toString(twoSum(nums2, 6)));
+        System.out.println(Arrays.toString(twoSum(nums3, 6)));
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            } else {
+                map.put(target - nums[i], i);
+            }
+        }
+        return null;
+    }
+}
--- inflearn/src/main/java/problem_01_to_10/_03TwoSum_2.java
+++ inflearn/src/main/java/problem_01_to_10/_03TwoSum_2.java
@@ -16,8 +16,8 @@ public class _03TwoSum_2 {
      */
 
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 18;
+        int[] nums = {3, 3};
+        int target = 6;
         
         System.out.println(Arrays.toString(solve(nums, target)));
     }
